d2ea878c14ca1b09b44a0eacb0312bb5aca5fad5
Mr Adrish <105520344+MrAdrish18@users.noreply.github.com>
2022-06-06 14:45:22 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -4,30 +4,30 @@ import aiohttp
 from os import getenv
 from dotenv import load_dotenv
 
-# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫)
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@AdrishOwner)
 load_dotenv()
 que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @AdrishNetwork
 API_HASH = getenv("API_HASH", "XXXXX")
 API_ID = int(getenv("API_ID", "XXXXX"))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
 ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
-BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/c6e1041c6c9a12913f57a.png")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/bcac4ce2a546c8012ebfa.jpg")
 BOT_NAME = getenv("BOT_NAME", "XXXXX")
 BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
 BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
-OWNER_NAME = getenv("OWNER_NAME", "𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "AdityaHalder")
-SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/adityabots/tgmusicbot")
+OWNER_NAME = getenv("OWNER_NAME", "Adrish X God")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "AdrishOwner")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/mradrish18/adrishxmusic")
 STRING_SESSION = getenv("STRING_SESSION", "session")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5356564375").split()))
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AdityaDiscus")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/AdityaServer")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5303726708").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AdrishMusicSupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/AdrishMusicUpdates")
 
-# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫) 
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (Adrish X God) 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/kaalxd")
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/TeamAdrish")
